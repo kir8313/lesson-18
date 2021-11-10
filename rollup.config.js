29d5500104cf2f69a1c2748994ce6db712d3f5d9
Kirill <kirill.arzamaskin@convergent.digital>
2021-11-10 23:26:52 +0300

    add project rollup

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import { babel } from '@rollup/plugin-babel';
+import styles from "rollup-plugin-styles";
+import image from 'rollup-plugin-img';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+
+
+export default {
+  input: './src/index.js',
+  output: {
+    file: './build/main.js',
+    format: 'cjs'
+  },
+
+  plugins: [
+    babel({ babelHelpers: 'bundled' }),
+    styles(),
+    image({
+      limit: 1000000
+    }),
+    serve({
+      open: true,
+      contentBase: './',
+      port: 8000,
+    }),
+    livereload(),
+  ],
+};
